8b03118b4934358e5c91e94ee2166e644261baf5
prathap <iamprathap2003@gmail.com>
2026-02-27 18:23:17 +0530

    version-3

--- Atm.java
+++ Atm.java
@@ -71,10 +71,67 @@
 	//System.out.println("your card is Block again try 24 houres"); 
   }
  }
-  public class Atm extends PaymentVersion2{
+   class PaymentVersion3 extends PaymentVersion2{
+	   
+	    public void payment()throws IOException{
+	  
+    Scanner scan = new Scanner(System.in);
+     double balance = 1000;
+	 int withra=0;
+	 int deposite=0;
+	 byte one=1;
+	 byte three = 4;
+	 while(one<three){
+	 System.out.println("set pin");
+     int set=scan.nextInt();
+     System.out.println("comfirm pin");
+     int comform = scan.nextInt();
+	 if(set==comform){
+	   System.out.println("creat password SucessFull");
+	   int roll;
+	   do{
+		   System.out.println("-->ATM menu<--");
+		   System.out.println("check balance (1)");
+		   System.out.println("withra amount (2)");
+		   System.out.println("Deposit amount (3)");
+		   System.out.println(" exit the ATM (4) ");
+		   roll=scan.nextInt();
+		   switch(roll){
+		    case 1:
+		   System.out.println("your Current balance :" +balance);
+		    break;
+			case 2:
+			  System.out.println("enter the Withdraw amount");
+			  withra =scan.nextInt();
+			  balance-=withra;
+			  System.out.println("withdraw SucessFull ");
+			  break;
+			  case 3:
+			  System.out.println("Deposit amount");
+			  deposite=scan.nextInt();
+			  balance+=deposite;
+			  System.out.println("Deposite SucessFull ");
+			  break;
+		   case 4:
+		   System.out.println("Thankyou for visit ");
+		   break;
+		   }
+	   }while(roll!=4);
+	   break;
+	 }
+     else{
+		 one++;
+	   System.out.println("wrong password");
+	   System.out.println("again  time  "+ (three-one));
+	   scan.nextLine();
+	  }
+     } 
+	}  
+   }
+  public class Atm extends PaymentVersion3{
   
   public static void main(String[]args){
-        PaymentVersion2 obj = new Atm();
+        PaymentVersion3 obj = new Atm();
            try{
 			obj.payment();   
 		   }
