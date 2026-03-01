2373ebf64694421cd8a263798b56627f31471487
prathap <iamprathap2003@gmail.com>
2026-03-01 15:14:40 +0530

    final-version

--- Atm.java
+++ Atm.java
@@ -128,15 +128,96 @@
      } 
 	}  
    }
-  public class Atm extends PaymentVersion3{
+   class PaymentVersion4 extends PaymentVersion3{
+	   
+	   public void payment()throws IOException{
+	  
+    Scanner scan = new Scanner(System.in);
+     double balance = 1000;
+	 int withra=0;
+	 int deposite=0;
+	 int pin=0;
+	 byte one=1;
+	 byte three = 4;
+	 while(one<three){
+	 System.out.println("set pin");
+     int set=scan.nextInt();
+     System.out.println("comfirm pin");
+     int comform = scan.nextInt();
+	 if(set==comform){
+		 pin=set;
+	   System.out.println("creat password SucessFull");
+	   int roll;
+	   do{
+		   System.out.println("-->ATM menu<--");
+		   System.out.println("check balance (1)");
+		   System.out.println("withra amount (2)");
+		   System.out.println("Deposit amount (3)");
+		   System.out.println("Change password (4)");
+		   System.out.println(" exit the ATM (5) ");
+		   roll=scan.nextInt();
+		   switch(roll){
+		    case 1:
+		   System.out.println("your Current balance :" +balance);
+		    break;
+			case 2:
+			  System.out.println("enter the Withdraw amount");
+			  withra =scan.nextInt();
+			  if(withra<=balance){
+			  balance-=withra;
+			  System.out.println("withdraw SucessFull ");
+			  }
+			  else{
+				System.out.println("Insufficient balance");  
+			  }
+			  break;
+			  case 3:
+			  System.out.println("Deposit amount");
+			  deposite=scan.nextInt();
+			  balance+=deposite;
+			  System.out.println("Deposite SucessFull ");
+			  break;
+			  case 4:
+			  System.out.println("enter the old pin");
+			  int oldpin=scan.nextInt();
+			  if(oldpin==pin){
+				  System.out.println("enter the new pin");
+			   int newpin=scan.nextInt();
+			    pin=newpin;
+				System.out.println("Pin change SucessFull");
+			  }
+			  else{
+				System.out.println("wrong old pin");				
+			  }
+			  break;
+		   case 5:
+		   System.out.println("Thankyou for visit ");
+		   break;
+		   default :
+		   System.out.println("invaild choies");
+		   }
+	   }while(roll!=5);
+	   break;
+	 }
+     else{
+		 one++;
+	   System.out.println("wrong password");
+	   System.out.println("again  time  "+ (three-one));
+	   scan.nextLine();
+	  }
+     } 
+	}  
+   }
+	   
+  public class Atm extends PaymentVersion4{
   
   public static void main(String[]args){
-        PaymentVersion3 obj = new Atm();
+        PaymentVersion4 obj = new Atm();
            try{
 			obj.payment();   
 		   }
 		   catch(Exception e){
-			   System.out.println(e);
+			   System.out.println("invaild input");
 		   }
     } 
   }
